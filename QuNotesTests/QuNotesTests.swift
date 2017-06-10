958d466148790574daea8483e2eb37d6a98df418
Alexander Guschin <alexguschin.work@gmail.com>
2017-06-10 09:45:32 +0300

    Initial setup

---
+++ QuNotesTests/QuNotesTests.swift
@@ -0,0 +1,23 @@
+//
+//  QuNotesTests.swift
+//  QuNotesTests
+//
+//  Created by Alexander Guschin on 09.06.17.
+//  Copyright © 2017 Alexander Guschin. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import QuNotes
+
+class QuNotesTests: QuickSpec {
+    
+    override func spec() {
+        describe("it works") {
+            
+            it("should work") {
+                expect(true).to(beTrue())
+            }
+        }
+    }
+}
