7fa656a7e5d9ae5428ba546198b8a36f8befe8cc
Alexander Guschin <alexguschin.work@gmail.com>
2017-06-11 18:39:33 +0300

    Add Note with NoteSpec

---
+++ QuNotes/Entity/Note.swift
@@ -0,0 +1,9 @@
+//
+// Created by Alexander Guschin on 11.06.17.
+// Copyright (c) 2017 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+}
---
+++ QuNotes/Entity/NoteSpec.swift
@@ -0,0 +1,20 @@
+//
+// Created by Alexander Guschin on 11.06.17.
+// Copyright (c) 2017 Alexander Guschin. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Result
+
+class NoteSpec: QuickSpec {
+    override func spec() {
+
+        var note: Note!
+
+        beforeEach {
+            note = Note()
+        }
+    }
+}
+
