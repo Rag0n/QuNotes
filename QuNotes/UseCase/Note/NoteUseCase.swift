73a595dfb001c0b281bb4ffba8d1c4108e9ea5e0
Alexander Guschin <alexguschin.work@gmail.com>
2017-06-29 21:35:47 +0300

    Implement updateNote method

--- QuNotes/UseCase/Note/NoteUseCase.swift
+++ QuNotes/UseCase/Note/NoteUseCase.swift
@@ -12,12 +12,27 @@ protocol HasNoteUseCase {
 class NoteUseCase {
     private var notes = [Note]()
 
-    func addNote(withContent content: String) {
+    func addNote(withContent content: String) -> Note {
         let newNote = Note(content: content)
         notes.append(newNote)
+        return newNote
     }
 
     func getAllNotes() -> [Note] {
         return notes
     }
+
+    func updateNote(_ note: Note, newContent: String) -> Note {
+        removeNote(note)
+        return Note(content: newContent)
+
+    }
+
+    private func removeNote(_ note: Note) {
+        guard let indexOfRemovedNote = notes.index(of: note) else {
+            return;
+        }
+        notes.remove(at: indexOfRemovedNote)
+    }
 }
+
--- QuNotes/UseCase/Note/NoteUseCaseSpec.swift
+++ QuNotes/UseCase/Note/NoteUseCaseSpec.swift
@@ -29,6 +29,24 @@ class NoteUseCaseSpec: QuickSpec {
                 let allNotes = useCase.getAllNotes()
                 expect(allNotes.first?.content).to(equal("note fixture"))
             }
+
+            it("returns created note") {
+                let createdNote = useCase.addNote(withContent: "note fixture");
+                expect(createdNote.content).to(equal("note fixture"))
+            }
+        }
+
+        describe("-updateNote:") {
+            var existingNote: Note!
+
+            beforeEach {
+                existingNote = useCase.addNote(withContent: "note fixture")
+            }
+
+            it("returns note with new content") {
+                let updatedNote = useCase.updateNote(existingNote, newContent: "new note fixture");
+                expect(updatedNote.content).to(equal("new note fixture"))
+            }
         }
     }
 }
