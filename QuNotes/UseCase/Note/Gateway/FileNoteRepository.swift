659e6958877aeb661a76e4fc8676a3033fd2eee1
Alexander Guschin <alexguschin.work@gmail.com>
2017-07-04 22:47:38 +0300

    Create FileNoteRepository & spec files

---
+++ QuNotes/UseCase/Note/Gateway/FileNoteRepository.swift
@@ -0,0 +1,9 @@
+//
+//  FileNoteRepository.swift
+//  QuNotes
+//
+//  Created by Alexander Guschin on 04.07.17.
+//  Copyright © 2017 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
---
+++ QuNotes/UseCase/Note/Gateway/FileNoteRepositorySpec.swift
@@ -0,0 +1,9 @@
+//
+//  FileNoteRepositorySpec.swift
+//  QuNotesTests
+//
+//  Created by Alexander Guschin on 04.07.17.
+//  Copyright © 2017 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
