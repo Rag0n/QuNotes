aa19ab86cb31d6d99e08f075682aef54199de485
Alexander Guschin <alexguschin.work@gmail.com>
2017-08-26 23:53:48 +0300

    Add Coordinator protocol

---
+++ QuNotes/UI/Common/Protocols/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  QuNotes
+//
+//  Created by Alexander Guschin on 26.08.17.
+//  Copyright © 2017 Alexander Guschin. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator {
+    var rootViewController: UIViewController { get }
+}
