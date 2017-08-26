4a9e939d2935ade50bb2cfb94fd95820aade31cd
Alexander Guschin <alexguschin.work@gmail.com>
2017-08-26 23:48:53 +0300

    Update NotePad dependency

--- Carthage/Checkouts/Notepad/Notepad/Extensions.swift
+++ Carthage/Checkouts/Notepad/Notepad/Extensions.swift
@@ -43,10 +43,7 @@ extension String {
     ///
     /// - returns: The equivalent NSRange.
     func nsRange(from range: Range<String.Index>) -> NSRange {
-        let from = range.lowerBound.samePosition(in: utf16)
-        let to = range.upperBound.samePosition(in: utf16)
-        return NSRange(location: utf16.distance(from: utf16.startIndex, to: from),
-                       length: utf16.distance(from: from, to: to))
+        return NSRange(range, in: self)
     }
 
     /// Converts a String to a NSRegularExpression.
