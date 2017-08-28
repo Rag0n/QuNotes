701bef7c819378bc9749675b9c28736373e74c78
Alexander Guschin <alexguschin.work@gmail.com>
2017-08-28 22:33:26 +0300

    Create empty Theme and ThemeManager

---
+++ QuNotes/UI/Theme/DefaultTheme.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultTheme.swift
+//  QuNotes
+//
+//  Created by Alexander Guschin on 28.08.17.
+//  Copyright © 2017 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+
+struct DefaultTheme {
+
+}
---
+++ QuNotes/UI/Theme/ThemeManager.swift
@@ -0,0 +1,15 @@
+//
+//  ThemeManager.swift
+//  QuNotes
+//
+//  Created by Alexander Guschin on 28.08.17.
+//  Copyright © 2017 Alexander Guschin. All rights reserved.
+//
+
+import UIKit
+
+struct ThemeManager {
+    static func applyThemeForWindow(window: UIWindow) {
+        let defaultTheme = DefaultTheme()
+    }
+}
