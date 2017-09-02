b16e8f61c091da90ef7f4454d60aac84207a9c24
Alexander Guschin <alexguschin.work@gmail.com>
2017-09-02 21:00:13 +0300

    Add composition operator

--- QuNotes/Utility/Operators.swift
+++ QuNotes/Utility/Operators.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2017 Alexander Guschin. All rights reserved.
 //
 
+// MARK: Pipe
+
 precedencegroup RightApplyPrecedence {
     associativity: right
     higherThan: AssignmentPrecedence
@@ -18,16 +20,37 @@ precedencegroup LeftApplyPrecedence {
     lowerThan: TernaryPrecedence
 }
 
-///// Pipe Backward | Applies the function to its left to an argument on its right.
+/// Pipe Backward | Applies the function to its left to an argument on its right.
 infix operator <| : RightApplyPrecedence
 
 func <| <U, T>(function: ((T) -> U), value: T) -> U {
     return function(value)
 }
 
-///// Pipe forward | Applies an argument on the left to a function on the right.
+/// Pipe forward | Applies an argument on the left to a function on the right.
 infix operator |> : LeftApplyPrecedence
 
 func |> <T, U>(value: T, function: ((T) -> U)) -> U {
     return function(value)
+}
+
+// MARK: Composition
+
+precedencegroup CompositionPrecedence {
+    associativity: right
+    higherThan: DefaultPrecedence
+}
+
+/// Right-to-Left Composition
+infix operator <<< : CompositionPrecedence
+
+func <<< <T1, T2, T3> (left: @escaping (T2)->T3, right: @escaping (T1)->T2) -> (T1)->T3 {
+    return { (t1: T1) -> T3 in return left(right(t1)) }
+}
+
+/// Left-to-Right Composition
+infix operator >>> : CompositionPrecedence
+
+func >>> <T1, T2, T3> (left: @escaping (T1)->T2, right: @escaping (T2)->T3) -> (T1)->T3 {
+    return { (t1: T1) -> T3 in return right(left(t1)) }
 }
