ada42cd7254a31cbf1b9797721d899e929e67867
Alexander Guschin <alexguschin.work@gmail.com>
2017-09-21 22:15:47 +0300

    Add LibraryViewController

---
+++ QuNotes/UI/Library/LibraryViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LibraryViewController.swift
+//  QuNotes
+//
+//  Created by Alexander Guschin on 21.09.17.
+//  Copyright © 2017 Alexander Guschin. All rights reserved.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+    
+}
