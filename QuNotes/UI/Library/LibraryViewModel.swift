a8a9aca8c567d3450a4a4d3b1eed25e239aaa769
Alexander Guschin <alexguschin.work@gmail.com>
2017-09-24 21:32:15 +0300

    Add LibraryViewModel

---
+++ QuNotes/UI/Library/LibraryViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  LibraryViewModel.swift
+//  QuNotes
+//
+//  Created by Alexander Guschin on 24.09.17.
+//  Copyright © 2017 Alexander Guschin. All rights reserved.
+//
+
+struct LibraryViewModel {
+    let notebooks: [String]
+}
