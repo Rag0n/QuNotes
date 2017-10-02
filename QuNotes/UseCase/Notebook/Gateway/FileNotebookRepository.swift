faea7456c7bf24cdf13b76afe4c5dafb91e5d18c
Alexander Guschin <alexguschin.work@gmail.com>
2017-10-02 23:07:21 +0300

    Implement FileNotebookRepository's getAll method

--- QuNotes/UseCase/Notebook/Gateway/FileNotebookRepository.swift
+++ QuNotes/UseCase/Notebook/Gateway/FileNotebookRepository.swift
@@ -9,11 +9,19 @@
 import Foundation
 import Result
 
+enum FileNotebookRepositoryError: Error {
+    case failedToFindDocumentDirectory
+    case failedToCreateFile
+}
+
 class FileNotebookRepository: NotebookRepository {
     // MARK: - API
 
+    var fileManager: FileManager!
+    var fileReader: FileReaderService!
+
     func getAll() -> Result<[Notebook], AnyError> {
-        return .success([])
+        return Result(try notebooksFromNotebookFiles())
     }
 
     func save(notebook: Notebook) -> Result<Notebook, AnyError> {
@@ -22,5 +30,31 @@ class FileNotebookRepository: NotebookRepository {
 
     func delete(notebook: Notebook) -> Result<Notebook, AnyError> {
         return .success(notebook)
+    }
+
+    // MARK: - Private
+
+    private lazy var encoder: JSONEncoder = {
+        let enc = JSONEncoder()
+        enc.outputFormatting = .prettyPrinted
+        return enc
+    }()
+    private lazy var decoder = JSONDecoder()
+
+    private func notebooksFromNotebookFiles() throws -> [Notebook] {
+        return try notebookFiles().map(notebookFromFile)
+    }
+
+    private func notebookFiles() throws -> [URL] {
+        guard let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            throw FileNotebookRepositoryError.failedToFindDocumentDirectory
+        }
+        let documentDirectoryContent = try fileManager.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: nil, options: [])
+        return documentDirectoryContent.filter { $0.pathExtension == "qvnotebook" }
+    }
+
+    private func notebookFromFile(fileURL: URL) throws -> Notebook {
+        let data = try fileReader.dataFrom(fileURL: fileURL)
+        return try decoder.decode(Notebook.self, from: data)
     }
 }
