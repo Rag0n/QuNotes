059cd9028ecddcea505bbffbe51f29de22054568
Alexander Guschin <alexguschin.work@gmail.com>
2017-10-03 23:16:13 +0300

    Fix spec for FileNotebookRepository's save
    Fix was needed because we are saving notebook's
    data to meta.json file

--- QuNotes/UseCase/Notebook/Gateway/FileNotebookRepositorySpec.swift
+++ QuNotes/UseCase/Notebook/Gateway/FileNotebookRepositorySpec.swift
@@ -110,40 +110,68 @@ class FileNotebookRepositorySpec: QuickSpec {
                 }
                 """
 
-                var fileManagerSpy: ReturningURLsAndSuccessfullyCreatingFileFileManagerSpy!
+                var fileManagerSpy: ReturningURLsAndFailingToCreateDirectoryFileManagerSpy!
 
                 beforeEach {
-                    fileManagerSpy = ReturningURLsAndSuccessfullyCreatingFileFileManagerSpy()
+                    fileManagerSpy = ReturningURLsAndFailingToCreateDirectoryFileManagerSpy()
                     notebookRepository.fileManager = fileManagerSpy
                 }
 
-                it("writes correct content to file") {
-                    _ = notebookRepository.save(notebook: notebook)
-                    let stringFromPassedData = String(data: fileManagerSpy.passedData!, encoding: .utf8)
-                    expect(stringFromPassedData).to(equal(expectedString))
-                }
-
-                it("writes to correct file path") {
+                it("creates notebook directory with correct file path") {
                     _ = notebookRepository.save(notebook: notebook)
                     expect(fileManagerSpy.passedPath).to(equal("Documents/notebookUuid.qvnotebook"))
                 }
 
-                context("when fileManager fails to create file") {
+                context("when fileManager fails to create directory") {
                     beforeEach {
-                        notebookRepository.fileManager = ReturningURLsAndFailingToCreateFileFileManagerSpy()
+                        fileManagerSpy = ReturningURLsAndFailingToCreateDirectoryFileManagerSpy()
+                        notebookRepository.fileManager = fileManagerSpy
                     }
 
                     it("returns result with error") {
                         let error = notebookRepository.save(notebook: notebook).error
-                        let underlyingError = (error!.error) as? FileNotebookRepositoryError
-                        expect(underlyingError).to(equal(FileNotebookRepositoryError.failedToCreateFile))
+                        let underlyingError = (error!.error) as NSError
+                        expect(underlyingError).to(equal(fileManagerSpy.thrownError))
                     }
                 }
 
-                context("when fileManager successfully creates file") {
-                    it("returns result with saved note") {
-                        let savedNotebook = notebookRepository.save(notebook: notebook).value
-                        expect(savedNotebook).to(equal(notebook))
+                context("when fileManager successfully creates directory") {
+
+                    var fileManagerSpy: ReturningURLsCreatingDirectoryAndSuccessfullyCreatingFileFileManagerSpy!
+
+                    beforeEach {
+                        fileManagerSpy = ReturningURLsCreatingDirectoryAndSuccessfullyCreatingFileFileManagerSpy()
+                        notebookRepository.fileManager = fileManagerSpy
+                    }
+
+                    it("creates notebook meta file") {
+                        _ = notebookRepository.save(notebook: notebook)
+                        expect(fileManagerSpy.passedPath).to(equal("Documents/notebookUuid.qvnotebook/meta.json"))
+                    }
+
+                    it("writes correct content to file") {
+                        _ = notebookRepository.save(notebook: notebook)
+                        let stringFromPassedData = String(data: fileManagerSpy.passedData!, encoding: .utf8)
+                        expect(stringFromPassedData).to(equal(expectedString))
+                    }
+
+                    context("when fileManager fails to create file") {
+                        beforeEach {
+                            notebookRepository.fileManager = ReturningURLsCreatingDirectoryAndFailingToCreateFileFileManagerSpy()
+                        }
+
+                        it("returns result with error") {
+                            let error = notebookRepository.save(notebook: notebook).error
+                            let underlyingError = (error!.error) as? FileNotebookRepositoryError
+                            expect(underlyingError).to(equal(FileNotebookRepositoryError.failedToCreateFile))
+                        }
+                    }
+
+                    context("when fileManager successfully creates file") {
+                        it("returns result with saved note") {
+                            let savedNotebook = notebookRepository.save(notebook: notebook).value
+                            expect(savedNotebook).to(equal(notebook))
+                        }
                     }
                 }
             }
@@ -178,5 +206,26 @@ class NotebookDummyFileReaderSpy: FileReaderSpy {
     override func data() -> Data {
         let encoder = JSONEncoder()
         return try! encoder.encode(notebookDummy)
+    }
+}
+
+class ReturningURLsAndCreatingDirectoryFileManagerSpy: ReturningURLsFileManagerStub {
+    private(set) var passedPath: String?
+    private(set) var thrownError = NSError(domain: "domain", code: 0, userInfo: nil)
+
+    override func createDirectory(atPath path: String, withIntermediateDirectories createIntermediates: Bool, attributes: [String : Any]? = nil) throws {
+        passedPath = path
+        throw thrownError
+    }
+}
+class ReturningURLsAndFailingToCreateDirectoryFileManagerSpy: ReturningURLsAndCreatingDirectoryFileManagerSpy {}
+class ReturningURLsCreatingDirectoryAndFailingToCreateFileFileManagerSpy: ReturningURLsAndCreatingFileFileManagerSpy {
+    override func createDirectory(atPath path: String, withIntermediateDirectories createIntermediates: Bool, attributes: [String : Any]? = nil) throws {
+    }
+}
+class ReturningURLsCreatingDirectoryAndSuccessfullyCreatingFileFileManagerSpy: ReturningURLsCreatingDirectoryAndFailingToCreateFileFileManagerSpy {
+    override func createFile(atPath path: String, contents data: Data?, attributes attr: [String : Any]? = nil) -> Bool {
+        _ = super.createFile(atPath: path, contents: data, attributes: attr)
+        return true
     }
 }
