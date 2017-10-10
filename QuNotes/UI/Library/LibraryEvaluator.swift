aa826ea8f04ea186e6bfe6b73ceb68fa1084fa07
Alexander Guschin <alexguschin.work@gmail.com>
2017-10-10 22:03:31 +0300

    Refactor LibraryEvaluator

--- QuNotes/UI/Library/LibraryEvaluator.swift
+++ QuNotes/UI/Library/LibraryEvaluator.swift
@@ -25,80 +25,81 @@ extension LibraryEvaluator {
 
 extension LibraryEvaluator {
     static func evaluateController(event: LibraryViewControllerEvent, model: LibraryCoordinatorModel) -> LibraryEvaluatorResult {
+        var actions: [LibraryCoordinatorAction] = []
+        let updates: [LibraryViewControllerUpdate] = []
+
         switch event {
         case .addNotebook:
-            let actions = [LibraryCoordinatorAction.addNotebook]
-            return LibraryEvaluatorResult(updates: [], actions: actions, model: model)
+            actions = [LibraryCoordinatorAction.addNotebook]
         case .deleteNotebook(let index):
             let notebook = model.notebooks[index]
-            let actions = [LibraryCoordinatorAction.deleteNotebook(notebook: notebook)]
-            return LibraryEvaluatorResult(updates: [], actions: actions, model: model)
+            actions = [LibraryCoordinatorAction.deleteNotebook(notebook: notebook)]
         case .selectNotebook(let index):
             let notebook = model.notebooks[index]
-            let actions = [LibraryCoordinatorAction.showNotes(forNotebook: notebook)]
-            return LibraryEvaluatorResult(updates: [], actions: actions, model: model)
+            actions = [LibraryCoordinatorAction.showNotes(forNotebook: notebook)]
         case .updateNotebook(let index, let title):
             let notebook = model.notebooks[index]
-            let actions = [LibraryCoordinatorAction.updateNotebook(notebook: notebook, title: title)]
-            return LibraryEvaluatorResult(updates: [], actions: actions, model: model)
+            actions = [LibraryCoordinatorAction.updateNotebook(notebook: notebook, title: title)]
         }
+
+        return LibraryEvaluatorResult(updates: updates, actions: actions, model: model)
     }
 }
 
 extension LibraryEvaluator {
     static func evaluateUseCase(event: NotebookUseCaseEvent, model: LibraryCoordinatorModel) -> LibraryEvaluatorResult {
+        let actions: [LibraryCoordinatorAction] = []
+        var updates: [LibraryViewControllerUpdate] = []
+        var newModel = model
+
         switch event {
         case .didUpdateNotebooks(let notebooks):
-            let sortedNotebooks = notebooks.sorted(by: { $0.name < $1.name })
-            let notebookViewModels = sortedNotebooks.map {
-                return NotebookCellViewModel(title: $0.name, isEditable: $0 == model.editingNotebook)
-            }
-            let updates = [LibraryViewControllerUpdate.updateAllNotebooks(notebooks: notebookViewModels)]
-            let model = LibraryCoordinatorModel(notebooks: sortedNotebooks, editingNotebook: nil)
-            return LibraryEvaluatorResult(updates: updates, actions: [], model: model)
+            let sortedNotebooks = notebooks.sorted(by: defaultNotebookSorting)
+            let notebookViewModels = viewModels(fromNotebooks: sortedNotebooks, editingNotebook: model.editingNotebook)
+            updates = [LibraryViewControllerUpdate.updateAllNotebooks(notebooks: notebookViewModels)]
+            newModel = LibraryCoordinatorModel(notebooks: sortedNotebooks, editingNotebook: nil)
         case .didAddNotebook(let notebook):
             let updatedNotebooks = model.notebooks + [notebook]
-            let sortedNotebooks = updatedNotebooks.sorted(by: { $0.name < $1.name })
-            let notebookViewModels = sortedNotebooks.map {
-                return NotebookCellViewModel(title: $0.name, isEditable: $0 == notebook)
-            }
+            let sortedNotebooks = updatedNotebooks.sorted(by: defaultNotebookSorting)
+            let notebookViewModels = viewModels(fromNotebooks: updatedNotebooks, editingNotebook: notebook)
             let indexOfNewNotebook = sortedNotebooks.index(of: notebook)!
-            let updates = [LibraryViewControllerUpdate.addNotebook(index: indexOfNewNotebook, notebooks: notebookViewModels)]
-            let newModel = LibraryCoordinatorModel(notebooks: sortedNotebooks, editingNotebook: notebook)
-            return LibraryEvaluatorResult(updates: updates, actions: [], model: newModel)
+            updates = [LibraryViewControllerUpdate.addNotebook(index: indexOfNewNotebook, notebooks: notebookViewModels)]
+            newModel = LibraryCoordinatorModel(notebooks: sortedNotebooks, editingNotebook: notebook)
         case .didDelete(let notebook):
             let indexOfDeletedNotebook = model.notebooks.index(of: notebook)!
-            var updatedNotebooks = model.notebooks
-            updatedNotebooks.remove(at: indexOfDeletedNotebook)
-            let notebookViewModels = updatedNotebooks.map {
-                return NotebookCellViewModel(title: $0.name, isEditable: false)
-            }
-            let updates = [LibraryViewControllerUpdate.deleteNotebook(index: indexOfDeletedNotebook, notebooks: notebookViewModels)]
-            let newModel = LibraryCoordinatorModel(notebooks: updatedNotebooks, editingNotebook: model.editingNotebook)
-            return LibraryEvaluatorResult(updates: updates, actions: [], model: newModel)
+            let updatedNotebooks = model.notebooks.removeWithoutMutation(at: indexOfDeletedNotebook)
+            let notebookViewModels = viewModels(fromNotebooks: updatedNotebooks)
+            updates = [LibraryViewControllerUpdate.deleteNotebook(index: indexOfDeletedNotebook, notebooks: notebookViewModels)]
+            newModel = LibraryCoordinatorModel(notebooks: updatedNotebooks, editingNotebook: model.editingNotebook)
         case .didUpdate(let notebook):
             let indexOfUpdatedNotebook = model.notebooks.index(of: notebook)!
             var updatedNotebooks = model.notebooks
             updatedNotebooks[indexOfUpdatedNotebook] = notebook
-            updatedNotebooks = updatedNotebooks.sorted(by: { $0.name < $1.name })
-            let notebookViewModels = updatedNotebooks.map {
-                return NotebookCellViewModel(title: $0.name, isEditable: false)
-            }
-            let updates = [LibraryViewControllerUpdate.updateAllNotebooks(notebooks: notebookViewModels)]
-            let newModel = LibraryCoordinatorModel(notebooks: updatedNotebooks, editingNotebook: nil)
-            return LibraryEvaluatorResult(updates: updates, actions: [], model: newModel)
+            updatedNotebooks = updatedNotebooks.sorted(by: defaultNotebookSorting)
+            let notebookViewModels = viewModels(fromNotebooks: updatedNotebooks)
+            updates = [LibraryViewControllerUpdate.updateAllNotebooks(notebooks: notebookViewModels)]
+            newModel = LibraryCoordinatorModel(notebooks: updatedNotebooks, editingNotebook: nil)
         case .failedToAddNotebook(let error):
-            let errorString = String(describing: error)
+            let _ = String(describing: error)
             // TODO: Add error update
-            return LibraryEvaluatorResult(updates: [], actions: [], model: model)
         case .failedToDeleteNotebook(let error):
-            let errorString = String(describing: error)
+            let _ = String(describing: error)
             // TODO: Add error update
-            return LibraryEvaluatorResult(updates: [], actions: [], model: model)
         case .failedToUpdateNotebook(let error):
-            let errorString = String(describing: error)
+            let _ = String(describing: error)
             // TODO: Add error update
-            return LibraryEvaluatorResult(updates: [], actions: [], model: model)
         }
+
+        return LibraryEvaluatorResult(updates: updates, actions: actions, model: newModel)
+    }
+
+    private static func viewModels(fromNotebooks: [Notebook], editingNotebook: Notebook? = nil) -> [NotebookCellViewModel] {
+        return fromNotebooks.map {
+            NotebookCellViewModel(title: $0.name, isEditable: $0 == editingNotebook)
+        }
+    }
+
+    private static func defaultNotebookSorting(leftNotebook: Notebook, rightNotebook: Notebook) -> Bool {
+        return leftNotebook.name < rightNotebook.name
     }
 }
