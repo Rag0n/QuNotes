2e14018d58d7c803370305a1534d61e928778f51
Alexander Guschin <alexguschin.work@gmail.com>
2017-10-14 23:10:09 +0300

    Implement NoteCoordinator's evaluateNoteUseCase

--- QuNotes/UI/Note/NoteEvaluator.swift
+++ QuNotes/UI/Note/NoteEvaluator.swift
@@ -19,9 +19,15 @@ extension UI.Note {
 
     static func evaluateController(event: ViewControllerEvent, model: Model) -> EvaluatorResult {
         var actions: [Action] = []
-        let updates: [ViewControllerUpdate] = []
+        var updates: [ViewControllerUpdate] = []
 
         switch event {
+        case .didLoad:
+            updates = [
+                .updateTitle(title: model.note.title),
+                .updateContent(content: model.note.content),
+                .showTags(tags: model.note.tags)
+            ]
         case let .changeContent(newContent):
             actions = [.updateContent(content: newContent)]
         case let .changeTitle(newTitle):
@@ -37,20 +43,53 @@ extension UI.Note {
         return EvaluatorResult(updates: updates, actions: actions, model: model)
     }
 
-    static func evaluateNoteUseCase(event: NoteUseCaseEvent, model: Model) -> EvaluatorResult {
-        let actions: [Action] = []
+    static func evaluateNoteUseCase(event: CoordinatorEvent, model: Model) -> EvaluatorResult {
+        var actions: [Action] = []
         var updates: [ViewControllerUpdate] = []
         var newModel = model
 
         switch event {
-        case let .didUpdateNote(note):
-            updates = []
+        case let .didUpdateTitle(note):
+            updates = [.updateTitle(title: note.title)]
+            newModel = Model(note: note)
+        case let .didUpdateContent(note):
+            updates = [.updateContent(content: note.content)]
+            newModel = Model(note: note)
+        case let .didAddTag(note, tag):
+            updates = [.addTag(tag: tag)]
+            newModel = Model(note: note)
+        case let .didRemoveTag(note, tag):
+            updates = [.removeTag(tag: tag)]
+            newModel = Model(note: note)
         case let .didDeleteNote(note):
-            updates = []
-        case let .didFailToUpdateNote(note):
-            updates = []
-        case let .didFailToDeleteNote(note):
-            updates = []
+            actions = [.finish]
+        case let .didFailToUpdateTitle(error):
+            let errorMessage = error.error.localizedDescription
+            updates = [
+                .updateTitle(title: model.note.title),
+                .showError(error: "Failed to update note's title", message: errorMessage)
+            ]
+        case let .didFailToUpdateContent(error):
+            let errorMessage = error.error.localizedDescription
+            updates = [
+                .updateContent(content: model.note.content),
+                .showError(error: "Failed to update note's content", message: errorMessage)
+            ]
+        case let .didFailToAddTag(error):
+            let errorMessage = error.error.localizedDescription
+            updates = [
+                .showTags(tags: model.note.tags),
+                .showError(error: "Failed to add tag", message: errorMessage)
+            ]
+        case let .didFailToRemoveTag(error):
+            let errorMessage = error.error.localizedDescription
+            updates = [
+                .showTags(tags: model.note.tags),
+                .showError(error: "Failed to remove tag", message: errorMessage)
+            ]
+        case let .didFailToDeleteNote(error):
+            let errorMessage = error.error.localizedDescription
+            updates = [.showError(error: "Failed to delete note", message: errorMessage)]
         }
 
         return EvaluatorResult(updates: updates, actions: actions, model: newModel)
