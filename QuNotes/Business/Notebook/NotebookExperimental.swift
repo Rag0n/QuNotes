143a01bccb80b9e6f6e0fd3c2a9aa48d2453af49
Alexander Guschin <alexguschin.work@gmail.com>
2017-10-30 23:50:58 +0300

    Add experimental notebook model & evaluator

--- QuNotes/Business/Notebook/NotebookExperimental.swift
+++ QuNotes/Business/Notebook/NotebookExperimental.swift
@@ -6,3 +6,87 @@
 //  Copyright © 2017 Alexander Guschin. All rights reserved.
 //
 
+extension Experimental {
+    enum Notebook {}
+}
+
+extension Experimental.Notebook {
+    struct Model: Codable {
+        let uuid: String
+        let name: String
+        let notes: [Experimental.Note.Model]
+    }
+
+    enum Action {
+        case updateModel(model: Model)
+    }
+
+    enum InputEvent {
+        case changeName(newName: String)
+    }
+
+    enum ResultEvent {
+        case didChangeName(newName: String)
+    }
+
+    struct Evaluator {
+        let actions: [Action]
+        let model: Model
+
+        init(model: Model) {
+            self.model = model
+            actions = []
+        }
+
+        func evaluate(event: InputEvent) -> Evaluator {
+            var actions: [Action] = []
+            var newModel = model
+
+            switch event {
+            case let .changeName(newName):
+                let updatedModel = Model(uuid: model.uuid, name: newName, notes: model.notes)
+                actions = [.updateModel(model: updatedModel)]
+            }
+
+            return Evaluator(actions: actions, model: newModel)
+        }
+
+        func evaluate(event: ResultEvent) -> Evaluator {
+            var actions: [Action] = []
+            var newModel = model
+
+            switch event {
+            case let .didChangeName(newName):
+                return Evaluator(actions: actions, model: newModel)
+            }
+
+            return Evaluator(actions: actions, model: newModel)
+        }
+
+        fileprivate init(actions: [Action], model: Model) {
+            self.actions = actions
+            self.model = model
+        }
+    }
+}
+
+extension Experimental.Notebook.Model: Equatable {
+    static func ==(lhs: Experimental.Notebook.Model, rhs: Experimental.Notebook.Model) -> Bool {
+        return (
+            lhs.uuid == rhs.uuid &&
+            lhs.name == rhs.name &&
+            lhs.notes == rhs.notes
+        )
+    }
+}
+
+// MARK: - Action Equtable
+extension Experimental.Notebook.Action: Equatable {}
+
+func ==(lhs: Experimental.Notebook.Action, rhs: Experimental.Notebook.Action) -> Bool {
+    switch (lhs, rhs) {
+    case (.updateModel(let lModel), .updateModel(let rModel)):
+        return lModel == rModel
+    default: return false
+    }
+}
--- QuNotes/Business/Notebook/NotebookExperimentalSpec.swift
+++ QuNotes/Business/Notebook/NotebookExperimentalSpec.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2017 Alexander Guschin. All rights reserved.
 //
 
-import Foundation
+import Quick
+import Nimble
+import Result
+
+class NotebookExperimantalSpec: QuickSpec {
+    override func spec() {
+        let model = Experimental.Notebook.Model(uuid: "uuid", name: "name", notes: [])
+        var e: Experimental.Notebook.Evaluator!
+
+        beforeEach {
+            e = Experimental.Notebook.Evaluator(model: model)
+        }
+
+        describe("-evaluate:") {
+            var event: Experimental.Notebook.InputEvent!
+
+            context("when receiving changeName event") {
+                let expectedModel = Experimental.Notebook.Model(uuid: "uuid", name: "new name", notes: [])
+
+                beforeEach {
+                    event = .changeName(newName: "new name")
+                }
+
+                it("has updateModel action with new name") {
+                    expect(e.evaluate(event: event).actions[0])
+                        .to(equal(.updateModel(model: expectedModel)))
+                }
+            }
+        }
+    }
+}
