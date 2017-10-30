27f5c95bcc2f0b9ff3ff456a432b1f1c71ff259a
Alexander Guschin <alexguschin.work@gmail.com>
2017-10-30 23:50:37 +0300

    Add experimental note model

--- QuNotes/Business/Note/NoteExperimental.swift
+++ QuNotes/Business/Note/NoteExperimental.swift
@@ -6,3 +6,24 @@
 //  Copyright © 2017 Alexander Guschin. All rights reserved.
 //
 
+extension Experimental {
+    enum Note {}
+}
+
+extension Experimental.Note {
+    struct Model: Codable {
+        let uuid: String
+        let title: String
+        let content: String
+    }
+}
+
+extension Experimental.Note.Model: Equatable {
+    static func ==(lhs: Experimental.Note.Model, rhs: Experimental.Note.Model) -> Bool {
+        return (
+            lhs.uuid == rhs.uuid &&
+            lhs.title == rhs.title &&
+            lhs.content == rhs.content
+        )
+    }
+}
