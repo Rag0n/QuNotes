5cf9bc307c7217bacbd82e86ed794178e75df3e4
Alexander Guschin <alexguschin.work@gmail.com>
2017-11-01 23:32:19 +0300

    Add Note's meta & content data types

--- QuNotes/Business/Note/NoteExperimental.swift
+++ QuNotes/Business/Note/NoteExperimental.swift
@@ -11,12 +11,23 @@ extension Experimental {
 }
 
 extension Experimental.Note {
-    struct Model: Codable {
+    struct Model {
         let uuid: String
         let title: String
         let content: String
     }
+
+    struct Meta: Codable {
+        let uuid: String
+        let title: String
+    }
+
+    struct Content: Codable {
+        let content: String
+    }
 }
+
+// MARK: Datatypes equatable
 
 extension Experimental.Note.Model: Equatable {
     static func ==(lhs: Experimental.Note.Model, rhs: Experimental.Note.Model) -> Bool {
@@ -25,5 +36,20 @@ extension Experimental.Note.Model: Equatable {
             lhs.title == rhs.title &&
             lhs.content == rhs.content
         )
+    }
+}
+
+extension Experimental.Note.Meta: Equatable {
+    static func ==(lhs: Experimental.Note.Meta, rhs: Experimental.Note.Meta) -> Bool {
+        return (
+            lhs.uuid == rhs.uuid &&
+            lhs.title == rhs.title
+        )
+    }
+}
+
+extension Experimental.Note.Content: Equatable {
+    static func ==(lhs: Experimental.Note.Content, rhs: Experimental.Note.Content) -> Bool {
+        return lhs.content == rhs.content
     }
 }
