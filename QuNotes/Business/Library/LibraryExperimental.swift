592f85cfa213c45571b6c3ee0d0677d2b0440149
Alexander Guschin <alexguschin.work@gmail.com>
2017-11-03 22:39:54 +0300

    Add empty Library & library spec files

---
+++ QuNotes/Business/Library/LibraryExperimental.swift
@@ -0,0 +1,22 @@
+//
+//  LibraryExperimental.swift
+//  QuNotes
+//
+//  Created by Alexander Guschin on 03.11.2017.
+//  Copyright © 2017 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+
+extension Experimental {
+    enum Library {}
+}
+
+extension Experimental.Library {
+}
+
+// MARK: Model API
+
+// MARK: Datatypes equtable
+
+// MARK: - Action Equtable
---
+++ QuNotes/Business/Library/LibraryExperimentalSpec.swift
@@ -0,0 +1,16 @@
+//
+//  LibraryExperimentalSpec.swift
+//  QuNotesTests
+//
+//  Created by Alexander Guschin on 03.11.2017.
+//  Copyright © 2017 Alexander Guschin. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Result
+
+class libraryExperimantalSpec: QuickSpec {
+    override func spec() {
+    }
+}
