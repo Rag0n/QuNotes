941f92a8379f6741ec86ff518c2a7473431d87b1
Alexander Guschin <alexguschin.work@gmail.com>
2017-11-04 22:23:23 +0300

    Implement Library createFile evaluator

--- QuNotes/Business/Library/LibraryExperimental.swift
+++ QuNotes/Business/Library/LibraryExperimental.swift
@@ -13,10 +13,68 @@ extension Experimental {
 }
 
 extension Experimental.Library {
+    struct Model {
+        let notebooks: [Experimental.Notebook.Model]
+    }
+
+    enum Action {
+        case createFile(url: URL, content: Codable)
+    }
+
+    enum InputEvent {
+        case addNotebook(notebook: Experimental.Notebook.Model)
+    }
+
+    struct Evaluator {
+        let actions: [Action]
+        let model: Model
+
+        init(model: Model) {
+            self.model = model
+            actions = []
+        }
+
+        func evaluate(event: InputEvent) -> Evaluator {
+            var actions: [Action] = []
+            var newModel = model
+
+            switch (event) {
+            case let .addNotebook(notebook):
+                newModel = Model(notebooks: model.notebooks + [notebook])
+                let fileURL = notebook.noteBookMetaURL()
+                let fileContent = Experimental.Notebook.Meta(uuid: notebook.uuid, name: notebook.name)
+                actions = [.createFile(url: fileURL, content: fileContent)]
+            }
+
+            return Evaluator(actions: actions, model: newModel)
+        }
+
+        private init(actions: [Action], model: Model) {
+            self.model = model
+            self.actions = actions
+        }
+    }
 }
 
 // MARK: Model API
 
 // MARK: Datatypes equtable
+
+extension Experimental.Library.Model: Equatable {
+    static func ==(lhs: Experimental.Library.Model, rhs: Experimental.Library.Model) -> Bool {
+        return lhs.notebooks == rhs.notebooks
+    }
+}
+
+extension Experimental.Library.Action: Equatable {
+    static func ==(lhs: Experimental.Library.Action, rhs: Experimental.Library.Action) -> Bool {
+        switch (lhs, rhs) {
+        case (.createFile(let lURL, let lContent as Experimental.Notebook.Meta),
+              .createFile(let rURL, let rContent as Experimental.Notebook.Meta)):
+            return (lURL == rURL) && (lContent == rContent)
+        default: return false
+        }
+    }
+}
 
 // MARK: - Action Equtable
