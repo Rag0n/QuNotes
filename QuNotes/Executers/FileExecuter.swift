83d226fd8b1efa7d3b8f14942fff88c2287ae889
Alexander Guschin <alexguschin.work@gmail.com>
2017-11-10 23:27:38 +0300

    Add createFile method to FileExecuter

--- QuNotes/Executers/FileExecuter.swift
+++ QuNotes/Executers/FileExecuter.swift
@@ -7,7 +7,37 @@
 //
 
 import Foundation
+import Result
 
 class FileExecuter {
+    // MARK: - API
+
+    func createFile<T: Encodable>(atURL url: URL, content: T) -> Error? {
+        let writeContent = writeToURL <| url.appendedToDocumentsURL()
+        return writeContent <| dataFromContent <| content
+    }
+
+    // MARK: - Private
     
+    private lazy var encoder: JSONEncoder = {
+        let enc = JSONEncoder()
+        enc.outputFormatting = .prettyPrinted
+        return enc
+    }()
+
+    private func dataFromContent<T: Encodable>(_ content: T) -> Result<Data, AnyError> {
+        return Result(try encoder.encode(content))
+    }
+
+    private func writeToURL(_ url: URL) -> (Result<Data, AnyError>) -> Error? {
+        return { result in
+            guard let data = result.value else { return result.error?.error }
+            do {
+                try data.write(to: url)
+                return nil
+            } catch {
+                return error
+            }
+        }
+    }
 }
