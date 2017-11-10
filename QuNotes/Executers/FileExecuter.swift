21a4a4cba67b5d24aebcff032f801f9d46f2e532
Alexander Guschin <alexguschin.work@gmail.com>
2017-11-10 20:44:38 +0300

    Add empty fileExecuter

---
+++ QuNotes/Executers/FileExecuter.swift
@@ -0,0 +1,13 @@
+//
+//  FileExecuter.swift
+//  QuNotes
+//
+//  Created by Alexander Guschin on 10.11.2017.
+//  Copyright © 2017 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+
+class FileExecuter {
+    
+}
