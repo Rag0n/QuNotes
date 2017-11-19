5888b219a50cf86cab9f06f700f5a9f280995aaa
Alexander Guschin <alexguschin.work@gmail.com>
2017-11-19 18:16:07 +0300

    Integrate sourcery into project

---
+++ QuNotes/Utility/Sourcery/SourceryProtocols.swift
@@ -0,0 +1,9 @@
+//
+//  SourceryProtocols.swift
+//  QuNotes
+//
+//  Created by Alexander Guschin on 19.11.2017.
+//  Copyright © 2017 Alexander Guschin. All rights reserved.
+//
+
+protocol AutoEquatable {}
