6f40f5473bfdeb990165388320778d461702e7dc
Alexander Guschin <alexguschin.work@gmail.com>
2017-11-22 19:31:05 +0300

    Add equalDiff matcher for arrays

--- QuNotes/Utility/Nimble/EqualDiffMatcher.swift
+++ QuNotes/Utility/Nimble/EqualDiffMatcher.swift
@@ -27,3 +27,22 @@ public func equalDiff<T: Equatable>(_ expectedValue: T?) -> Predicate<T> {
         return PredicateResult(bool: receivedValue == expectedValue, message: ExpectationMessage.fail("Found difference for " + diff(expectedValue, receivedValue).joined(separator: ", ")))
     }
 }
+
+public func equalDiff<T: Equatable>(_ expectedValue: [T]?) -> Predicate<[T]> {
+    return Predicate.define { actualExpression in
+        let receivedValue = try actualExpression.evaluate()
+
+        if receivedValue == nil {
+            var message = ExpectationMessage.fail("")
+            if let expectedValue = expectedValue {
+                message = ExpectationMessage.expectedCustomValueTo("equal <\(expectedValue)>", "nil")
+            }
+            return PredicateResult(status: .fail, message: message)
+        }
+        if expectedValue == nil {
+            return PredicateResult(status: .fail, message: ExpectationMessage.fail("").appendedBeNilHint())
+        }
+
+        return PredicateResult(bool: receivedValue! == expectedValue!, message: ExpectationMessage.fail("Found difference for " + diff(expectedValue, receivedValue).joined(separator: ", ")))
+    }
+}
