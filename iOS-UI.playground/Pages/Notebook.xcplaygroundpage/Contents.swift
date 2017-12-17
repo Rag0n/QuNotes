4beecee5dd74cb606b3690db1db2028c1ac5dbb7
Alexander Guschin <alexguschin.work@gmail.com>
2017-12-17 17:26:15 +0300

    Add Notebook page to playground

---
+++ iOS-UI.playground/Pages/Notebook.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+//: [Previous](@previous)
+
+import UIKit
+import PlaygroundSupport
+import QuNotesUI
+
+let controller = NotebookViewController { (event) in
+    print(event)
+}
+
+let navigationController = UINavigationController(rootViewController: controller)
+ThemeManager.applyTheme(forView: navigationController.view)
+
+let (parent, _) = playgroundControllers(device: .phone4inch, orientation: .portrait, child: navigationController)
+
+let frame = parent.view.frame
+PlaygroundPage.current.liveView = parent
+parent.view.frame = frame
+
+controller.perform(effect: .updateAllNotes(notes: ["First note", "Second note", "Third note"]))
+controller.perform(effect: .updateTitle(title: "Notebook title"))
+
