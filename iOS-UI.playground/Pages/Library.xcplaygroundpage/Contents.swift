f973d8980d7823d589383bc039f70e5a143eb0bb
Alexander Guschin <alexguschin.work@gmail.com>
2017-12-18 20:02:28 +0300

    Add library page to playground

---
+++ iOS-UI.playground/Pages/Library.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+import PlaygroundSupport
+import QuNotesUI
+
+let controller = LibraryViewController { (event) in
+    print(event)
+}
+
+let navigationController = UINavigationController(rootViewController: controller)
+ThemeManager.applyTheme(forView: navigationController.view)
+
+let (parent, _) = playgroundControllers(device: .phone4inch, orientation: .portrait, child: navigationController)
+
+let frame = parent.view.frame
+PlaygroundPage.current.liveView = parent
+parent.view.frame = frame
+
+let firstNotebook = Library.NotebookViewModel(title: "First notebook title", isEditable: false)
+let secondNotebook = Library.NotebookViewModel(title: "Second notebook title", isEditable: false)
+let thirdNotebook = Library.NotebookViewModel(title: "Third notebook title", isEditable: false)
+controller.perform(effect: .updateAllNotebooks(notebooks: [firstNotebook,
+                                                           secondNotebook,
+                                                           thirdNotebook]))
+
+//: [Next](@next)
