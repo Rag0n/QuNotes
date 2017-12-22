551b82b96fbc41c5e4dbde47370ef5f671b0e1a2
Alexander Guschin <alexguschin.work@gmail.com>
2017-12-22 21:21:30 +0300

    Refactor NoteCoordinator

--- QuNotes/Note/NoteCoordinator.swift
+++ QuNotes/Note/NoteCoordinator.swift
@@ -10,24 +10,9 @@ import Prelude
 
 extension Note {
     final class CoordinatorImp: Coordinator {
-        // MARK: - Coordinator
-        
         var viewController: UIViewController {
             return noteViewController
         }
-
-        // MARK: - Life cycle
-
-        fileprivate var fileExecuter: FileExecuterType {
-            return AppEnvironment.current.fileExecuter
-        }
-        fileprivate let navigationController: NavigationController
-        fileprivate var evaluator: Evaluator
-        fileprivate var noteEvaluator: Core.Note.Evaluator
-
-        fileprivate lazy var noteViewController: NoteViewController = {
-            return NoteViewController(withDispatch: dispatch)
-        }()
 
         init(withNavigationController navigationController: NavigationController, note: Core.Note.Meta,
              isNewNote: Bool, notebook: Core.Notebook.Meta) {
@@ -38,30 +23,30 @@ extension Note {
 
         // MARK: - Private
 
-        fileprivate func dispatch(event: ViewEvent) {
+        private func dispatch(event: ViewEvent) {
             event |> evaluator.evaluate |> updateEvaluator
         }
 
-        fileprivate func dispatch(event: CoordinatorEvent) {
+        private func dispatch(event: CoordinatorEvent) {
             event |> evaluator.evaluate |> updateEvaluator
         }
 
-        fileprivate func dispatchToNote(event: Core.Note.Event) {
+        private func dispatchToNote(event: Core.Note.Event) {
             event |> noteEvaluator.evaluate |> updateNote
         }
 
-        fileprivate func updateEvaluator(evaluator: Evaluator) {
+        private func updateEvaluator(evaluator: Evaluator) {
             self.evaluator = evaluator
             evaluator.actions.forEach(perform)
             evaluator.effects.forEach(noteViewController.perform)
         }
 
-        fileprivate func updateNote(note: Core.Note.Evaluator) {
+        private func updateNote(note: Core.Note.Evaluator) {
             self.noteEvaluator = note
             note.effects.forEach(perform)
         }
 
-        fileprivate func perform(action: Action) {
+        private func perform(action: Action) {
             switch action {
             case let .updateTitle(title):
                 dispatchToNote <| .changeTitle(newTitle: title)
@@ -81,11 +66,30 @@ extension Note {
             }
         }
 
-        fileprivate func perform(effect: Core.Note.Effect) {
+        private func perform(effect: Core.Note.Effect) {
             switch effect {
-            default:
+            case let .updateTitle(note, url):
+                break
+            case let .updateContent(content, url):
+                break
+            case let .addTag(note, url):
+                break
+            case let .removeTag(note, url):
                 break
             }
         }
+
+        // MARK: State
+
+        private var fileExecuter: FileExecuterType {
+            return AppEnvironment.current.fileExecuter
+        }
+        private let navigationController: NavigationController
+        private var evaluator: Evaluator
+        private var noteEvaluator: Core.Note.Evaluator
+
+        private lazy var noteViewController: NoteViewController = {
+            return NoteViewController(withDispatch: dispatch)
+        }()
     }
 }
