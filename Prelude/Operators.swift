a967a600deee010cddb96c45eb185766ad4176b9
Alexander Guschin <alexguschin.work@gmail.com>
2018-01-30 22:57:31 +0300

    Change composition operator

--- Prelude/Operators.swift
+++ Prelude/Operators.swift
@@ -23,14 +23,14 @@ precedencegroup CompositionPrecedence {
     higherThan: LeftApplyPrecedence
 }
 
-precedencegroup LensCompositionPrecedence {
-    associativity: left
-    higherThan: LensSetPrecedence
-}
-
 precedencegroup LensSetPrecedence {
     associativity: left
     higherThan: CompositionPrecedence
+}
+
+precedencegroup LensCompositionPrecedence {
+    associativity: left
+    higherThan: LensSetPrecedence
 }
 
 // MARK: Pipe
@@ -51,14 +51,11 @@ public func |> <T, U>(value: T, function: ((T) -> U)) -> U {
 
 // MARK: Composition
 
-/// Right-to-Left Composition
-public func <| <T1, T2, T3> (left: @escaping (T2)->T3, right: @escaping (T1)->T2) -> (T1)->T3 {
-    return { (t1: T1) -> T3 in return left(right(t1)) }
-}
+/// Left-to-Right composition operator | Combines the functions to its left with the function on its right.
+infix operator >>> : CompositionPrecedence
 
-/// Left-to-Right Composition
-public func |>  <T1, T2, T3> (left: @escaping (T1)->T2, right: @escaping (T2)->T3) -> (T1)->T3 {
-    return { (t1: T1) -> T3 in return right(left(t1)) }
+public func >>>  <T1, T2, T3>(left: @escaping (T1) -> T2, right: @escaping (T2) -> T3) -> ((T1) -> T3) {
+    return { t1 in right(left(t1)) }
 }
 
 // MARK: Lens
